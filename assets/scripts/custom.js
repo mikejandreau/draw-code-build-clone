4d287c4e17daab3df73f15de9c52e7eb98d198a0
Mike Jandreau <mikejandreau@gmail.com>
2018-11-27 16:09:21 -0500

    Updated search script paths and styles

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,6 +1,60 @@
 // main.js
 
 
+// hasClass
+function hasClass(elem, className) {
+    return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+
+// addClass
+function addClass(elem, className) {
+    if (!hasClass(elem, className)) {
+        elem.className += ' ' + className;
+    }
+}
+
+// removeClass
+function removeClass(elem, className) {
+    var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0) {
+            newClass = newClass.replace(' ' + className + ' ', ' ');
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+}
+
+// toggleClass
+function toggleClass(elem, className) {
+    var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, " ") + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0) {
+            newClass = newClass.replace(" " + className + " ", " ");
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
+
+
+
+
+// Toggle search icon and content
+var searchToggle = document.querySelector('.search-toggle');
+    myInitialContent = document.querySelector('.initial-content');
+    mySearchContent = document.querySelector('.search-content');
+
+searchToggle.onclick = function() {
+  toggleClass(mySearchContent, 'is--visible');
+  toggleClass(myInitialContent, 'is--hidden');
+};
+
+
+
+
+
+
 
 // // toggle the mobile menu button open and closed states
 // var removeClass = true;
