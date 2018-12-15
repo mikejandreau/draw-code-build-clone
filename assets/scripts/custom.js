6a7e54033e4b5e654b9a556361af47bb16bab4c1
Mike Jandreau <mikejandreau@gmail.com>
2018-12-15 14:52:06 -0500

    Tweaks to scripts and styles

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,5 +1,7 @@
 // main.js
 
+// CLASS HELPERS
+// Vanilla JS class toggling scripts for use without jQuery
 
 // hasClass
 function hasClass(elem, className) {
@@ -42,13 +44,18 @@ function toggleClass(elem, className) {
 
 // Toggle search icon and content
 var searchToggle = document.querySelector('.search-toggle');
-    myInitialContent = document.querySelector('.main-content');
-    mySearchContent = document.querySelector('.search-content');
+    mainContent = document.querySelector('.main-content');
+    searchContent = document.querySelector('.search-content');
 
 searchToggle.onclick = function() {
-  toggleClass(mySearchContent, 'is--visible');
-  toggleClass(myInitialContent, 'is--hidden');
+  toggleClass(searchContent, 'is--visible');
+  toggleClass(mainContent, 'is--hidden');
 };
+
+// searchContent.onclick = function() {
+//   removeClass(searchContent, 'is--visible');
+//   addClass(mainContent, 'is--hidden');
+// };
 
 
 
@@ -59,122 +66,6 @@ searchToggle.onclick = function() {
 //   }
 //   removeClass = true;
 // });
-
-
-
-// // toggle the mobile menu button open and closed states
-// var removeClass = true;
-// $(".navbar-toggler").click(function () {
-// 	$(".navbar-toggler").toggleClass('is-active');
-// 	removeClass = false;
-// });
-
-// // ignore clicking the navbar
-// $(".navbar").click(function() {
-// 	removeClass = false;
-// });
-
-// // close nav if page is clicked
-// $("html").click(function () {
-// 	if (removeClass) {
-// 		$(".navbar-toggler").removeClass('is-active');
-// 		$('.collapse').collapse('hide');  
-// 	}
-// 	removeClass = true;
-// });
-
-// // close nav if link is clicked
-// $(".navbar a").click(function () {
-// 	if (removeClass) {
-// 		$(".navbar-toggler").removeClass('is-active');
-// 		$('.collapse').collapse('hide');  
-// 	}
-// 	removeClass = true;
-// });
-
-// // disable mobile nav for laptop and desktop
-// $(window).resize(function() {
-// 	if( $(this).width() > 767 ) {
-// 		$(".navbar-toggler").removeClass('is-active');
-// 		$('.collapse').collapse('hide');  
-// 	}
-// });
-
-// // show scrollup button after scrolling 300px
-// scrollToTopButton = document.getElementById("scrollUpButton");
-// var showScrollToTop = function () {
-// 	var y = window.scrollY;
-// 	if (y >= 300) {
-// 		scrollToTopButton.className = "scrollup show";
-// 	} else {
-// 		scrollToTopButton.className = "scrollup";
-// 	}
-// };
-// window.addEventListener("scroll", showScrollToTop);
-
-
-
-
-
-// // append caption to figure in fancybox gallery
-// $('[data-fancybox="images"]').fancybox({
-
-// 	// Enable infinite gallery navigation
-// 	loop: false,
-
-// 	// display counter at the top left corner
-// 	infobar: false,
-
-// 	// buttons to appear in the top right corner in toolbar (class="fancybox-toolbar"` element)
-// 	buttons: [
-// 		// "zoom",
-// 		//"share",
-// 		// "slideShow",
-// 		//"fullScreen",
-// 		//"download",
-// 		// "thumbs",
-// 		"close"
-// 	],
-// 	caption : function(instance,item) {
-// 		return $(this).closest('figure').find('figcaption').html();
-// 	}
-// });
-
-
-
-
-
-// // jquery easing
-// (function($) {
-//   "use strict"; // Start of use strict
-
-//   // Smooth scrolling using jQuery easing
-//   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-//     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-//       var target = $(this.hash);
-//       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//       if (target.length) {
-//         $('html, body').animate({
-//           scrollTop: (target.offset().top - 54)
-//         }, 1000, "easeInOutExpo");
-//         return false;
-//       }
-//     }
-//   });
-
-//   // Closes responsive menu when a scroll trigger link is clicked
-//   $('.js-scroll-trigger').click(function() {
-//     $('.navbar-collapse').collapse('hide');
-//   });
-
-//   // Activate scrollspy to add active class to navbar items on scroll
-//   $('body').scrollspy({
-//     target: '#mainNav',
-//     offset: 54
-//   });
-
-// })(jQuery); // End of use strict
-
 
 
 
@@ -290,15 +181,15 @@ window.addEventListener("scroll", showScrollToTop);
 
 
 
-// CONTACT FORM
-// If contact form is present, add action attribute and email
-// Shortened from => if (typeof(contactForm) != 'undefined' && contactForm != null) 
-var contactForm =  document.getElementById('contactform');
-if ( contactForm != null ) {
+// // CONTACT FORM
+// // If contact form is present, add action attribute and email
+// // Shortened from => if (typeof(contactForm) != 'undefined' && contactForm != null) 
+// var contactForm =  document.getElementById('contactform');
+// if ( contactForm != null ) {
 
-    contactForm.setAttribute('action', '//formspree.io/' + 'northeastcreativedesign' + '@' + 'gmail' + '.' + 'com');
+//     contactForm.setAttribute('action', '//formspree.io/' + 'northeastcreativedesign' + '@' + 'gmail' + '.' + 'com');
 
-}
+// }
 
 
 
@@ -322,21 +213,6 @@ $().fancybox({
   idleTime : false
 })
 
-// // Init Isotope
-// var $grid = $('.grid').isotope({
-//   // options
-// });
-
-// // Filter items on button click
-// $('.filter-button-group').on( 'click', 'button', function() {
-//   $grid.isotope({ filter: $(this).attr('data-filter') });
-// });
-
-
-
-
-
-
 
 
 // ISOTOPE FILTERING
@@ -344,12 +220,6 @@ $().fancybox({
 var isotopeWrapper =  document.querySelector('.grid');
 if ( isotopeWrapper != null ) {
 
-  // // init Isotope
-  // var iso = new Isotope( '.grid-row', {
-  //   itemSelector: '.preview-item',
-  //   layoutMode: 'fitRows'
-  // });
-  
   var iso;
 
   imagesLoaded( isotopeWrapper, function() {
@@ -391,10 +261,6 @@ if ( isotopeWrapper != null ) {
     });
   }
 }
-
-
-
-
 
 })(jQuery); // End of use strict
 
